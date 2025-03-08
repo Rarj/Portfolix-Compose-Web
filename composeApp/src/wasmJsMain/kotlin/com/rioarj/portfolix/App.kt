ddc472a645e9843dd9b3a4fc80a0018295ea3a24
Rio Arj <rio.arjuna@digits.id>
2025-03-08 17:17:46 +0700

    Create an App.kt file

---
+++ composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/App.kt
@@ -0,0 +1,14 @@
+package com.rioarj.portfolix
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import com.rioarj.portfolix.style.PoppinsTypography
+
+@Composable
+fun App() {
+    MaterialTheme(
+        typography = PoppinsTypography(),
+    ) {
+
+    }
+}
