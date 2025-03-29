ace2ea04595fd23d34fbe1416bb41b1cb5dffea0
Rio Arj <developer.arj@gmail.com>
2025-03-29 13:09:56 +0700

    Layouting AboutMe.kt page
    Signed-off-by: Rio Arj <developer.arj@gmail.com>

--- composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/page/AboutMe.kt
+++ composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/page/AboutMe.kt
@@ -5,19 +5,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rioarj.portfolix.component.HeaderPageUI
+import com.rioarj.portfolix.page.resume.education.EducationComponent
+import com.rioarj.portfolix.page.resume.education.EducationHelper
 import com.rioarj.portfolix.style.backgroundColor
 import com.rioarj.portfolix.style.orangeColor
 import com.rioarj.portfolix.style.subtitleColor
@@ -36,10 +40,8 @@ internal fun AboutMeUI(modifier: Modifier = Modifier) {
             subtitle = "Simplicity is the soul of efficiency",
         )
         Row(
-            modifier = Modifier
-                .wrapContentSize()
-                .padding(bottom = 100.dp)
-                .padding(horizontal = 100.dp)
+            modifier = Modifier.wrapContentSize().padding(bottom = 100.dp)
+                .padding(horizontal = 200.dp).clip(RoundedCornerShape(8.dp))
                 .background(backgroundColor)
         ) {
             Image(
@@ -48,7 +50,7 @@ internal fun AboutMeUI(modifier: Modifier = Modifier) {
                 contentDescription = "Profile Picture"
             )
             Column(
-                modifier = Modifier.fillMaxSize().padding(all = 64.dp),
+                modifier = Modifier.fillMaxSize().padding(top = 24.dp, start = 24.dp, end = 56.dp),
                 verticalArrangement = Arrangement.Center,
             ) {
                 Text(
@@ -57,14 +59,37 @@ internal fun AboutMeUI(modifier: Modifier = Modifier) {
                     fontSize = 56.sp,
                     fontWeight = FontWeight.Bold,
                 )
-                Spacer(modifier = Modifier.size(16.dp))
-                Text(
-                    text = "I’m Rio, an experienced Android Engineer with a diverse background spanning multiple industries. I’m passionate about learning and exploring new technologies to create innovative and impactful solutions.\n" + "\n" + "Alongside my technical expertise, I’m known for being a friendly team player and a natural cheerleader, always encouraging and supporting those around me.\n" + "\n" + "I thrive in environments where collaboration is key, and I love motivating my team to push boundaries and achieve meaningful, high-impact outcomes together.",
-                    color = subtitleColor,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Medium,
-                    letterSpacing = 0.7.sp,
-                )
+                Row {
+                    VerticalDivider(
+                        modifier = Modifier.clip(RoundedCornerShape(percent = 50)).height(190.dp),
+                        thickness = 4.dp,
+                        color = orangeColor,
+                    )
+                    Text(
+                        modifier = Modifier.padding(horizontal = 4.dp),
+                        text = "I’m Rio, an experienced Android Engineer with a diverse background spanning multiple industries. I’m passionate about learning and exploring new technologies to create innovative and impactful solutions.\n" + "\n" + "Alongside my technical expertise, I’m known for being a friendly team player and a natural cheerleader, always encouraging and supporting those around me.\n" + "\n" + "I thrive in environments where collaboration is key, and I love motivating my team to push boundaries and achieve meaningful, high-impact outcomes together.",
+                        color = subtitleColor,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        letterSpacing = 0.7.sp,
+                    )
+                }
+
+                val educations = EducationHelper.educations
+                Column(
+                    modifier = Modifier.padding(top = 24.dp, end = 24.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                ) {
+                    Text(
+                        text = "Study",
+                        color = orangeColor,
+                        fontSize = 56.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    repeat(educations.size) {
+                        EducationComponent(education = educations[it])
+                    }
+                }
             }
         }
     }
--- composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/page/resume/education/EducationComponent.kt
+++ composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/page/resume/education/EducationComponent.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -16,10 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rioarj.portfolix.style.orangeColor
+import com.rioarj.portfolix.style.subtitleColor
 
 @Composable
 internal fun EducationComponent(
@@ -28,14 +30,14 @@ internal fun EducationComponent(
 ) {
     Row(
         modifier = modifier,
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
     ) {
         Column(
-            modifier = Modifier.padding(horizontal = 16.dp).width(800.dp),
+            modifier = Modifier.wrapContentSize(),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start,
         ) {
+            Spacer(modifier = Modifier.size(12.dp))
+
             Row(
                 modifier = Modifier.wrapContentHeight(),
             ) {
@@ -52,6 +54,7 @@ internal fun EducationComponent(
                         text = education.name,
                         fontSize = 16.sp,
                         fontWeight = FontWeight.Bold,
+                        color = Color.White,
                     )
                     Text(
                         text = buildString {
@@ -61,11 +64,12 @@ internal fun EducationComponent(
                         },
                         fontSize = 16.sp,
                         fontWeight = FontWeight.Normal,
+                        color = subtitleColor,
                     )
                 }
             }
 
-            Spacer(modifier = Modifier.size(24.dp))
+            Spacer(modifier = Modifier.size(12.dp))
         }
     }
 }
