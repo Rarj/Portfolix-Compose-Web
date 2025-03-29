1d1bd6c5cbeae2461069cdfb99a287ae3abf84de
Rio Arj <developer.arj@gmail.com>
2025-03-29 15:14:14 +0700

    Enhancing HeaderPage.kt section
    Signed-off-by: Rio Arj <developer.arj@gmail.com>

--- composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/component/HeaderPage.kt
+++ composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/component/HeaderPage.kt
@@ -1,13 +1,18 @@
 package com.rioarj.portfolix.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -15,6 +20,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rioarj.portfolix.style.orangeColor
+import com.rioarj.portfolix.style.subtitleColor
 
 @Composable
 fun ColumnScope.HeaderPageUI(
@@ -22,26 +28,32 @@ fun ColumnScope.HeaderPageUI(
     subtitle: String,
 ) {
     this.apply {
-        Spacer(modifier = Modifier.size(24.dp))
-        Text(
-            text = title,
-            color = Color.Black,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-        )
-        Text(
-            text = subtitle,
-            color = Color.Black,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Normal,
-        )
+        Column(
+            modifier = Modifier.clip(RoundedCornerShape(50)).background(subtitleColor)
+                .padding(horizontal = 48.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.size(12.dp))
+            Text(
+                text = title,
+                color = Color.Black,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = subtitle,
+                color = Color.Black,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+            )
+            Spacer(modifier = Modifier.size(12.dp))
+        }
         Spacer(modifier = Modifier.size(8.dp))
         HorizontalDivider(
-            modifier = Modifier.width(100.dp)
-                .clip(RoundedCornerShape(percent = 50)),
+            modifier = Modifier.width(100.dp).clip(RoundedCornerShape(percent = 50)),
             color = orangeColor,
             thickness = 4.dp,
         )
-        Spacer(modifier = Modifier.size(24.dp))
     }
 }
