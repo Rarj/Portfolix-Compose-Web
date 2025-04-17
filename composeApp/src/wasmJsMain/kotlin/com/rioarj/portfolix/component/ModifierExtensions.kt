d2b7d6fef20f69b4c1034df2205f731bd67f3ef9
Rio Arj <developer.arj@gmail.com>
2025-04-18 01:15:01 +0700

    initial commit

--- composeApp/src/wasmJsMain/kotlin/com/rioarj/portfolix/component/ModifierExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.rioarj.portfolix.component
-
-import androidx.compose.runtime.Stable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-
-@Stable
-internal fun Modifier.mirror() = this.scale(scaleX = -1f, scaleY = 1f)
